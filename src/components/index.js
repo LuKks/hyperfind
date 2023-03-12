bb3dc14a42073697be3c4b09f062c74bf4d45d86
Manuel <bsasmanuelsilva@gmail.com>
2023-03-12 17:50:04 -0300

    Renamed PrintCore and PrintBlocks correctly

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
-import PrintBlocks from 'components/PrintCore'
-import PrintCore from 'components/PrintBlocks'
+import PrintCore from 'components/PrintCore'
+import PrintBlocks from 'components/PrintBlocks'
 import SpanSection from 'components/SpanSection'
 import SpanTitle from 'components/SpanTItle'
 import CustomInput from 'components/CustomInput'
