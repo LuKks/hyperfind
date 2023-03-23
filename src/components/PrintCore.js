dcbe19f0f9c01a84729500aa36fa5eca268d5c30
Manuel <bsasmanuelsilva@gmail.com>
2023-03-22 22:44:26 -0300

    Removed '<br />'

--- src/components/PrintCore.js
+++ src/components/PrintCore.js
@@ -4,15 +4,12 @@ function PrintCore ({ core, lookup }) {
   if (!core || !lookup) return null
 
   return (
-    <>
+    <section className='print-core-section'>
       <SpanTitle>Core</SpanTitle>
-      <br />
-
-      <SpanSection>ID</SpanSection> <span style={{ color: '#58a6ff' }}>{core.id}</span><br />
-      <SpanSection>Length</SpanSection> {core.length}<br />
-      <SpanSection>Peers</SpanSection> {core.peers.length}<br />
-      <br />
-    </>
+      <SpanSection>ID <span style={{ color: '#58a6ff' }}>{core.id}</span></SpanSection> 
+      <SpanSection>Length <span>{core.length}</span></SpanSection> 
+      <SpanSection>Peers <span>{core.peers.length}</span></SpanSection> 
+    </section>
   )
 }
 
